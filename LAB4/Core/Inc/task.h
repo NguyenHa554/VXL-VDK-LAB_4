65b7196d02de64c3544530ffd6ea5809e7c29a7e
unknown <nguyen.hadtscvd554@hcmut.edu.vn>
2024-11-19 09:24:06 +0700

    basic test scheduler

---
+++ LAB4/Core/Inc/task.h
@@ -0,0 +1,15 @@
+/*
+ * task.h
+ *
+ *  Created on: Nov 19, 2024
+ *      Author: ADMIN
+ */
+
+#ifndef INC_TASK_H_
+#define INC_TASK_H_
+
+#include "main.h"
+
+void test();
+
+#endif /* INC_TASK_H_ */
---
+++ LAB4/Core/Src/task.c
@@ -0,0 +1,13 @@
+/*
+ * task.c
+ *
+ *  Created on: Nov 19, 2024
+ *      Author: ADMIN
+ */
+
+#include "task.h"
+
+void test(){
+		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+	}
+
